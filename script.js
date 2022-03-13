f7cacb74188c66e34d5a06d277b38b15fd5b802f
Pranjal Nama <pranjalnama01@gmail.com>
2022-03-13 10:51:11 +0530

    added picture uploader

--- script.js
+++ script.js
@@ -25,3 +25,32 @@ function addNewAQField() {
 
 	aqOb.insertBefore(newNode, aqAddButtonOb);
 }
+
+const inpFile = document.getElementById("inpFile");
+const previewContainer = document.getElementById("imagePreview");
+const previewImage = previewContainer.querySelector(".image-preview__image");
+const previewDefaultText = previewContainer.querySelector(
+	".image-preview__default-text"
+);
+
+inpFile.addEventListener("change", function () {
+	const file = this.files[0];
+
+	if (file) {
+		const reader = new FileReader();
+
+		previewDefaultText.style.display = "none";
+		previewImage.style.display = "block";
+
+		reader.addEventListener("load", function () {
+			console.log(this);
+			previewImage.setAttribute("src", this.result);
+		});
+
+		reader.readAsDataURL(file);
+	} else {
+		previewDefaultText.style.display = null;
+		previewImage.style.display = null;
+		previewImage.setAttribute("src", "");
+	}
+});
